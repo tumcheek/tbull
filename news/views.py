97424bccb6d0510a4217ec60dd914f7bc75cf759
Danil <dania.tumanov@gmail.com>
2024-11-30 16:14:46 +0200

    Add swagger documentation for NewsListApiView

--- news/views.py
+++ news/views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import ListAPIView
 
 
@@ -5,9 +7,20 @@ from news.models import News
 from news.serializers import NewsSerializer
 
 
+
 class NewsListApiView(ListAPIView):
     queryset = News.objects.all()
     serializer_class = NewsSerializer
+
+    @swagger_auto_schema(manual_parameters=[openapi.Parameter(
+        name='last',
+        in_=openapi.IN_QUERY,
+        description='The number of last news',
+        type=openapi.TYPE_INTEGER,
+        required=False,
+    ), ])
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
     def get_queryset(self):
         last = self.request.GET.get('last', None)
